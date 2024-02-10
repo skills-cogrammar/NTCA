326e6329c79aa84ee2ac24b82545df281c29ee46
ChrisJ-HyperionDev <129095722+ChrisJ-HyperionDev@users.noreply.github.com>
2024-02-10 20:40:41 +0200

    Add files via upload

--- Lecture Slides/Code Examples/lecture 4.py
+++ Lecture Slides/Code Examples/lecture 4.py
@@ -1,45 +1,56 @@
 import random as rd
 
-def rock_paper_scissors(choice):
-    
-    games = 0
-    wins = 0
-    losses = 0
-    
+def rock_paper_scissors(choice, win, lose):
+
+    wins = win
+    losses = lose
+
     select = ['rock', 'paper', 'scissors']
     cpu_choice = rd.choice(select)
 
     if cpu_choice == choice:
-        return f"You chose {choice}, CPU chose {cpu_choice}. It's a tie!"
+        print(f"Score - WIN:{wins}, LOSE:{losses}")
+        return f"You chose {choice}, CPU chose {cpu_choice}. It's a tie!", wins, losses
     
-    elif cpu_choice == 'Rock' and choice == 'Paper':
+    elif cpu_choice == 'rock' and choice == 'paper':
         wins += 1
-        return f"You chose {choice}, CPU chose {cpu_choice}. You win!"
+        print(f"Score - WIN:{wins}, LOSE:{losses}")
+        return f"You chose {choice}, CPU chose {cpu_choice}. You win!", wins, losses
     
-    elif cpu_choice == 'Paper' and choice == 'Scissors':
+    elif cpu_choice == 'paper' and choice == 'scissors':
         wins += 1
-        return f"You chose {choice}, CPU chose {cpu_choice}. You win!"
+        print(f"Score - WIN:{wins}, LOSE:{losses}")
+        return f"You chose {choice}, CPU chose {cpu_choice}. You win!", wins, losses
+    
+    elif cpu_choice == 'scissors' and choice == 'rock':
+        wins += 1
+        print(f"Score - WIN:{wins}, LOSE:{losses}")
+        return f"You chose {choice}, CPU chose {cpu_choice}. You win!", wins, losses
 
+    else:
+        losses += 1
+        print(f"Score - WIN:{wins}, LOSE:{losses}")
+        return f"You chose {choice}, CPU chose {cpu_choice}. The CPU wins!", wins, losses
+
+total_wins = 0
+total_lose = 0
 
 while True:
     user_choice = input("Please enter either Rock, Paper or Scissors : ").lower()
-    outcome = rock_paper_scissors(user_choice)
+    outcome, total_wins, total_lose = rock_paper_scissors(user_choice, total_wins, total_lose)
 
     print(outcome)
     
     terminate = input("Go again? (Y/N) : ").upper()
     
-    if terminate == 'Y':
+    if terminate == 'N':
         break
     
-    elif terminate == 'N':
+    elif terminate == 'Y':
         continue
 
-
-# The other checks Nizaam was too lazy to create
-
+        
 def addition(x, y):
-
     value = x + y
     return value
 
@@ -47,10 +58,15 @@ x = int(input("Please enter a number : "))
 y = int(input("Please enter a number : "))
 
 total = addition(x, y)
-print(total * 4)
+print(total)
 
 
 def login(username, password):
+    '''
+    Function takes in two string values.
+    One for username and one for password.
+    Returns True if the user logged in or false if they didn't.
+    '''
 
     user = "admin" 
     security_word = "adm1n"
@@ -69,6 +85,7 @@ def login(username, password):
 
 
 print("Welcome! Please login with your details!")
+
 while True:
 
     user_name = input("Please enter your name :")
@@ -92,3 +109,5 @@ print(add_two(10))
 
 welcome_msg = lambda name : print(f"Welcome back {name}")
 welcome_msg("Bob")
+welcome_msg("Bill")
+
